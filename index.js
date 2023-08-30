f20e7376c74ec7548a225cbded028ff481bb5473
DGBFOUNDER <123794602+DGBFOUNDER@users.noreply.github.com>
2023-08-30 08:09:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,208 @@
+/************************************************************************
+
+App setup
+
+*************************************************************************/
+// force https
+if (location.protocol === "http:") location.protocol = "https:";
+
+// handle the service worker registration
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then((reg) => console.log("Service Worker registered", reg))
+    .catch((err) => console.error("Service Worker **not** registered", err));
+} else {
+  console.warn("Service Worker not supported in this browser");
+}
+
+/************************************************************************
+
+Set a few booleans, detect install
+
+*************************************************************************/
+// Set isInstalledPWA if we're in app mode 😎
+const isInstalledPWA = window.matchMedia("(display-mode: standalone)").matches;
+// Check the user agent for iOS & Android (only for minor fixes — don't rely on user agent!)
+const isIOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+const isAndroid = /android/i.test(navigator.userAgent);
+
+
+// add a helper class for ".show-for-installed" and ".show-for-browser"
+function showInstalledBlocks() {
+  if (!isInstalledPWA) {
+    document.querySelectorAll(".show-for-browser").forEach((el) => {
+      el.style.display = "block";
+    });
+    document.querySelectorAll(".show-for-installed").forEach((el) => {
+      el.style.display = "none";
+    });
+  } else {
+    document.querySelectorAll(".show-for-browser").forEach((el) => {
+      el.style.display = "none";
+    });
+    document.querySelectorAll(".show-for-installed").forEach((el) => {
+      el.style.display = "block";
+    });
+  }
+}
+
+showInstalledBlocks();
+
+/************************************************************************
+
+Feature: Install reminder
+
+*************************************************************************/
+// figure out if we should show the install nudges
+const installNudge = document.querySelector("#install-nudge");
+const closeButton = document.getElementById("close-button");
+const hideBanner = localStorage.getItem("hide-install-nudge");
+
+function handleCloseButton() {
+  localStorage.setItem("hide-install-nudge", true);
+  installNudge.style.display = "none";
+}
+
+if (!isInstalledPWA && installNudge && !hideBanner) {
+  installNudge.style.display = "block";
+  closeButton.addEventListener("click", handleCloseButton);
+} else {
+  installNudge.style.display = "none";
+}
+
+/************************************************************************
+
+Feature: Notifications
+
+*************************************************************************/
+// add notifcation settings here:
+const enablePushNotifications = false; // true to enable
+const pushServerBaseURL = ""; // your full push server URL
+const VAPID_PUBLIC_KEY = ""; // public key from push server
+
+// track permissions for the sake of badging
+let pushNotificationPermissionGranted = false;
+
+// grab notification elements
+const buttonNotifications = document.getElementById("button-notifications");
+
+if (buttonNotifications && enablePushNotifications) {
+  // set up event notification handlers
+  buttonNotifications.addEventListener("click", () => {
+    askNotificationPermission();
+  });
+  // execute our notification functions and set up the page elements
+  handlePermission();
+}
+
+// touchstart event clears any active badges
+window.addEventListener(
+  "touchstart",
+  (e) => {
+    clearBadge();
+  },
+  false
+);
+
+function handlePermission() {
+  // set the button and subsequent form to shown or hidden, depending on what the user answers
+  if ("Notification" in window && buttonNotifications) {
+    // Mobile Safari errors out without checking for the Notification obj first
+    if (Notification.permission !== "granted") {
+      buttonNotifications.style.display = "block";
+      pushNotificationPermissionGranted = true;
+    } else {
+      // this looks extra but it clears the button after accepting permission on iOS
+      buttonNotifications.style.display = "none";
+    }
+  }
+}
+
+function askNotificationPermission() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    console.log("This browser does not support notifications.");
+  } else {
+    Notification.requestPermission().then((permission) => {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        handlePermission();
+        subscribeToPush();
+        let notification = new Notification("Notifications enabled", {
+          body: "We're best friends now! (We'll say hi when there's news.)",
+          icon: "https://cdn.glitch.com/560ed5ed-9d00-433a-9ff9-7750d79d13da%2FGlitch_TeamAvatar.png?v=1624643105812",
+        });
+      }
+    });
+  }
+}
+
+async function subscribeToPush() {
+  const registration = await navigator.serviceWorker.getRegistration();
+  const subscription = await registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlB64ToUint8Array(VAPID_PUBLIC_KEY),
+  });
+  postToServer(`${pushServerBaseURL}/add-subscription`, subscription);
+}
+
+
+/************************************************************************
+
+Feature: Orientation changes
+
+*************************************************************************/
+
+// just an example of what can be done detecting orientation
+// sets up helper classes `.show-for-portrait` and `.show-for-landscape`
+// also good for taking video fullscreen, moving nav elements, etc.
+// note: most desktop browsers always say "landscape"
+function showOrientationBlocks() {
+  if (
+    screen.orientation.type == "portrait-primary" ||
+    screen.orientation.type == "portrait-secondary" // this means "upside down" lol
+  ) {
+    document.querySelectorAll(".show-for-portrait").forEach((el) => {
+      el.style.display = "block";
+    });
+    document.querySelectorAll(".show-for-landscape").forEach((el) => {
+      el.style.display = "none";
+    });
+  } else if (
+    screen.orientation.type == "landscape-primary" ||
+    screen.orientation.type == "landscape-secondary" // upsie downsies again
+  ) {
+    document.querySelectorAll(".show-for-portrait").forEach((el) => {
+      el.style.display = "none";
+    });
+    document.querySelectorAll(".show-for-landscape").forEach((el) => {
+      el.style.display = "block";
+    });
+  }
+}
+
+// fix for iPhone zoom issues after orientation changes
+// see: http://www.menucool.com/McMenu/prevent-page-content-zooming-on-mobile-orientation-change
+function rotateWithNoScale() {
+  let viewport = document.querySelector("meta[name=viewport]");
+  if (viewport) {
+    let content = viewport.getAttribute("content");
+    viewport.setAttribute("content", content + ", maximum-scale=1.0");
+    setTimeout(function () {
+      viewport.setAttribute("content", content);
+    }, 100);
+  }
+}
+
+// actually detect the orientation changes and reapply
+screen.orientation.addEventListener("change", function (e) {
+  if (isIOS) {
+    rotateWithNoScale();
+  }
+  showOrientationBlocks();
+});
+
+// show/hide orientation classes
+showOrientationBlocks();
 
