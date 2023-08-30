8947c21d91dbb1379e109aa980ed6e3a26e1b366
DGBFOUNDER <123794602+DGBFOUNDER@users.noreply.github.com>
2023-08-29 20:42:10 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,27 +7,10 @@
 // Specify what we want added to the cache for offline use
 self.addEventListener("install", (e) => {
   e.waitUntil(
+    // Give the cache a name
     caches.open("glitch-hello-installable-cache").then((cache) => {
-      return cache.addAll(["/", "/about.html", "/install.html", "/style.css", "/index.js"]);
-    })
-  );
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    // Delete outdated caches if needed
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => {
-            // Check if the cache name doesn't match the current cache
-            return cacheName !== "glitch-hello-installable-cache";
-          })
-          .map((cacheName) => {
-            // Delete outdated cache
-            return caches.delete(cacheName);
-          })
-      );
+      // Cache the homepage and stylesheets - add any assets you want to cache!
+      return cache.addAll(["/", "/style.css", "/index.js"]);
     })
   );
 });
@@ -63,5 +46,3 @@ self.addEventListener("push", (e) => {
   // Finally...
   event.waitUntil(Promise.all(promises));
 });
-
-
